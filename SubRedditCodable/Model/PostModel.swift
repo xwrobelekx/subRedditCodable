a0d5570491a64ec0044b75a35caaac04710fae75
Kamil Wrobel <wrobelk@live.com>
2018-09-04 18:26:28 -0600

    creates model

--- SubRedditCodable/Model/PostModel.swift
+++ SubRedditCodable/Model/PostModel.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+struct MainDictionary{
+    
+    let data: Datadictionary
+    
+    struct Datadictionary {
+        
+        let childern: childernsDictionary
+        
+        struct childernsDictionary {
+            
+            let data2 : dataDictionaty
+            
+            private enum CodingKeys: String, CodingKey {
+                case data2 = "data"
+            }
+            
+            struct dataDictionaty {
+                let title           : String
+                let upVotes         : Int
+                let postImage       : String
+                let numberOfComents : Int
+                
+                private enum CodingKeys: String, CodingKey {
+                    case title
+                    case upVotes         = "ups"
+                    case postImage       = "url"
+                    case numberOfComents = "num_comments"
+                }
+            }
+        }
+    }
+}
+
+//let post = MainDictionary(data: data)
+//let data = MainDictionary.Datadictionary(childern: childern)
+//let childern = MainDictionary.Datadictionary.childernsDictionary(data: data2)
+//let data2 = MainDictionary.Datadictionary.childernsDictionary.dataDictionaty(title: "title", upVotes: 234, postImage: "http:bulshit.com", numberOfComents: 34)
+
+
