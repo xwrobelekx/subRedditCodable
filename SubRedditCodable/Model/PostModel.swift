598ef54f62e79db5fbfc17f901c83d47c2e3ee62
Kamil Wrobel <wrobelk@live.com>
2018-09-05 16:54:19 -0600

    final commit

--- SubRedditCodable/Model/PostModel.swift
+++ SubRedditCodable/Model/PostModel.swift
@@ -8,38 +8,149 @@
 
 import Foundation
 
-struct MainDictionary{
+
+struct JSONDictionary: Codable {
+    let data: DataDictionary
     
-    let data: Datadictionary
     
-    struct Datadictionary {
+    struct DataDictionary: Codable{
+        let children: [PostDictionary]
         
-        let childern: childernsDictionary
         
-        struct childernsDictionary {
+        struct PostDictionary: Codable {
+            let post: Post
             
-            let data2 : dataDictionaty
-            
-            private enum CodingKeys: String, CodingKey {
-                case data2 = "data"
-            }
-            
-            struct dataDictionaty {
-                let title           : String
-                let upVotes         : Int
-                let postImage       : String
-                let numberOfComents : Int
-                
-                private enum CodingKeys: String, CodingKey {
-                    case title
-                    case upVotes         = "ups"
-                    case postImage       = "url"
-                    case numberOfComents = "num_comments"
-                }
+            private enum CodingKeys: String, CodingKey{
+                case post = "data"
             }
         }
     }
 }
+
+
+
+
+struct Post: Codable{
+    let title: String
+    let thumbnailEndpoint: String
+    let numberOfUpvotes: Int
+    let numberOfComments: Int
+    
+    private enum CodingKeys: String, CodingKey{
+        case title
+        case thumbnailEndpoint = "thumbnail"
+        case numberOfUpvotes = "ups"
+        case numberOfComments = "num_comments"
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct DataDictionary: Codable{
+//
+//    let title : String
+//    let ups: Int
+//    let num_comments: Int
+//    let url: String
+//
+//
+//    private enum CodableKeys: String, CodingKey{
+//        case title
+//        case upPosts = "ups"
+//        case numberOfComments = "num_comments"
+//        case postImage = "url"
+//    }
+//
+//}
+//
+//struct Number {
+//
+//    let number = Int
+//
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//struct Post: Decodable{
+//
+//    let data: [Datadictionary]
+//
+//    struct Datadictionary: Decodable {
+//
+//        let childern: childernsDictionary
+//
+//        struct childernsDictionary: Decodable {
+//
+//            let data2 : dataDictionaty
+//
+//            private enum CodingKeys: String, CodingKey {
+//                case data2 = "data"
+//            }
+//
+//            struct dataDictionaty: Decodable {
+//                let title           : String
+//                let upVotes         : Int
+//                let postImage       : String
+//                let numberOfComents : Int
+//
+//                private enum CodingKeys: String, CodingKey {
+//                    case title
+//                    case upVotes         = "ups"
+//                    case postImage       = "url"
+//                    case numberOfComents = "num_comments"
+//                }
+//            }
+//        }
+//    }
+//}
 
 //let post = MainDictionary(data: data)
 //let data = MainDictionary.Datadictionary(childern: childern)
